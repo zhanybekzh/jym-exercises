d5b0535b6a05a05c654eb28728ff6a0b4533923b
Zhandos <zh.zhanybek@gmail.com>
2024-11-13 23:16:22 +0500

    feat: add fetchdata function

---
+++ src/utils/fetchData.js
@@ -0,0 +1,21 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null; 
+  }
+};
