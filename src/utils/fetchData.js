c0762ccde411a88e8f7c624cbf56a68afb3e1b61
Zhandos <zh.zhanybek@gmail.com>
2024-11-16 04:13:49 +0500

    feat: add fetchdata util

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,6 +5,13 @@ export const exerciseOptions = {
     "x-rapidapi-host": "exercisedb.p.rapidapi.com",
   },
 };
+export const youTubeOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+    "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
 
 export const fetchData = async (url, options) => {
   try {
@@ -16,6 +23,6 @@ export const fetchData = async (url, options) => {
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
-    return null; 
+    return null;
   }
 };
